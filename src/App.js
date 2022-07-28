89c5a6128a1b32db01b579677e7198d48bf8ceea
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-07-28 22:07:50 +0300

    Added link to GitHub ad Netlify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
  export default function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Oksana Shvets and is{" "}
+        <a href="https://github.com/ok-sana-code/react-weather-app">
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://delightful-chebakia-49d210.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
