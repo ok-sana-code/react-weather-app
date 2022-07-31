680709285d81688bb1d669604697ec129bbe140a
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-07-31 23:49:08 +0300

    Made icon bigger

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="black"
-      size={45}
+      size={55}
       animate={true}
     />
   );
