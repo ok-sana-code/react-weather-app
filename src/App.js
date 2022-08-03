6166f24becb9e30d3d38b573c13aa921745b426c
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-08-03 12:00:19 +0300

    Added styles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <div>
-        <Weather defaultCity="Lviv"/>
+        <Weather defaultCity="Lviv" />
         <footer>
-          This project was coded by Oksana Shvets and is{" "}
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/oksana-shvets-548086244/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B%2FBTUHgVcQCuoq0g8k4QJ8Q%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Oksana Shvets
+          </a>{" "}
+          and is{" "}
           <a
             href="https://github.com/ok-sana-code/react-weather-app"
             target="_blank"
