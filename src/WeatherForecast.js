5b3cd9c459e272d90617ebd1bbefac2cbaaf2536
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-08-03 11:37:11 +0300

    Fixed a bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -27,6 +27,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay date={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
